41b2525e948210d901b77b40b3d482cb283c296b
10000-ki <10000ki6472@gmail.com>
2023-09-17 16:20:07 +0900

    resolve TwoSum

---
+++ week01/mk/TwoSum.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	result := twoSum([]int{-3, 0, 3, 6}, 0)
+	fmt.Println(result)
+}
+
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+	for index, value := range nums {
+		firstNum := target - value
+		firstNumIndex, exist := numMap[firstNum]
+		if exist {
+			secondNumIndex := index
+			return []int{firstNumIndex, secondNumIndex}
+		}
+		numMap[value] = index
+	}
+
+	return []int{}
+}
