4a47470a91f0a1a9b21123c9cf51f71e5ff88e10
z0 <ji9310@naver.com>
2023-10-07 14:52:05 +0900

    Create Solution1.kt

---
+++ week04/z0/Solution1.kt
@@ -0,0 +1,24 @@
+class Solution {
+    // 234 ms, 38.6 MB
+    // 로마숫자는 뒤로 갈수록 작아져야 함
+    // i-1가 i보다 큰 경우에는 뺴기를 수행함
+    fun romanToInt(s: String): Int {
+        val romanDict: Map<Char, Int> = mapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
+        )
+        val intList: MutableList<Int> = ArrayList(s.map { romanDict[it] as Int })
+        for (i in 1..intList.lastIndex) {
+            if(intList[i-1] < intList[i]) {
+                intList[i-1] = intList[i-1] * -1
+                intList[i] = intList[i] * 1
+            }
+        }
+        return intList.sum()
+    }
+}
