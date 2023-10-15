fe267a7f4783b14f3fbf08f62c38c9d6e5d2583d
10000-ki <10000ki6472@gmail.com>
2023-10-15 15:12:08 +0900

    use go lang has prefix algo

--- week05/mk/LongestCommonPrefix.go
+++ week05/mk/LongestCommonPrefix.go
@@ -3,36 +3,47 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"}))
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
 
 }
 
+func longestCommonPrefix(s []string) string {
+	result := s[0]
+	for i := 1; i < len(s); i++ {
+		for !strings.HasPrefix(s[i], result) {
+			result = result[:len(result)-1]
+		}
+	}
+	return result
+}
+
+/*
 func longestCommonPrefix(strs []string) string {
-	temp := strs[0]
 	result := strs[0]
 	for i := 1; i < len(strs); i++ {
 		current := strs[i]
 		compareLen := 0
-		if len(temp) < len(current) {
-			compareLen = len(temp)
+		if len(result) < len(current) {
+			compareLen = len(result)
 		} else {
 			compareLen = len(current)
 		}
 
 		sameIndex := 0
 		for j := 0; j < compareLen; j++ {
-			if temp[j] == current[j] {
+			if result[j] == current[j] {
 				sameIndex++
 			} else {
 				break
 			}
 		}
 		result = result[:sameIndex]
-		temp = result
 	}
 
 	return result
 }
+*/
