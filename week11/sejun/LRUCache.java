638958258f5ab8d40f418c963c9b559eadb0528a
pizzazaza <riding1516@gmail.com>
2024-01-09 00:39:42 +0900

    Create LRUCache.java

---
+++ week11/sejun/LRUCache.java
@@ -0,0 +1,40 @@
+class LRUCache {
+		private Map<Integer, Integer> lru;
+		private Set<Integer> history = new LinkedHashSet<>();
+		private Integer lruCapacity;
+
+
+		public LRUCache(int capacity) {
+			lruCapacity = capacity;
+			lru = new HashMap<>(capacity);
+		}
+
+		public int get(int key) {
+            if (lru.containsKey(key)) {
+				history.remove(key);
+				history.add(key);
+				
+				return lru.get(key);
+			}
+			
+			return -1;
+		}
+
+		public void put(int key, int value) {
+			if (lru.containsKey(key)) {
+				history.remove(key);
+				lru.remove(key);
+			}
+
+			
+			if (Objects.equals(lruCapacity, lru.size())) {
+				Iterator<Integer> iter = history.iterator();
+				Integer historyKey = (Integer) iter.next();
+				lru.remove(historyKey);
+				history.remove(historyKey);
+			}
+
+			lru.put(key, value);
+			history.add(key);
+		}
+	}
