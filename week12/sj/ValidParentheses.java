60dd28b4b4b171779ba5b75a34310b67d06bea4a
AngJoong <conquerorj@naver.com>
2024-01-15 00:13:17 +0900

    Create ValidParentheses.java

---
+++ week12/sj/ValidParentheses.java
@@ -0,0 +1,31 @@
+// 2ms, 41.16MB
+class Solution {
+    private Map<Character, Character> closeAndOpen = Map.of(
+		')', '(',
+		'}', '{',
+		']', '['
+	);
+
+	public boolean isValid(String s) {
+		char[] parentheses = s.toCharArray();
+		Stack<Character> bucket = new Stack<>();
+
+		for (char parenthesis : parentheses) {
+			Character open = closeAndOpen.get(parenthesis);
+			if (open == null) {
+				bucket.push(parenthesis);
+				continue;
+			}
+
+			if (bucket.empty()) {
+				return false;
+			} 
+
+            if (bucket.pop() != open) {
+                return false;
+            }
+		}
+
+		return bucket.empty();
+	}
+}
