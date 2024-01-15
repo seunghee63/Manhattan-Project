b097aa1920a6801385bdc3097d41ce0a104ae64e
10000-ki <10000ki6472@gmail.com>
2024-01-16 00:42:52 +0900

    add get..

--- week12/mk/lfu_cache.go
+++ week12/mk/lfu_cache.go
@@ -5,25 +5,23 @@ import "container/list"
 type LFUCache struct {
 	size     int
 	capacity int
-	counter     map[int]*list.List
-	items     map[int]*list.Element
+	counter  map[int]*list.List
+	items    map[int]*list.Element
 }
 
 type Item struct {
-	Key  int
-	Val  int
+	Key   int
+	Val   int
 	Count int
 }
-
 
 func Constructor(capacity int) LFUCache {
 	return LFUCache{
 		capacity: capacity,
-		counter:     make(map[int]*list.List),
-		items:     make(map[int]*list.Element),
+		counter:  make(map[int]*list.List),
+		items:    make(map[int]*list.Element),
 	}
 }
-
 
 func (cache *LFUCache) Get(key int) int {
 	item, exists := cache.items[key]
@@ -32,24 +30,31 @@ func (cache *LFUCache) Get(key int) int {
 	}
 
 	value := item.Value.(Item)
-	cache.incrementCount(value, key)
+	cache.increaseCount(value, key)
 
 	return value.Val
 }
 
-func (cache *LFUCache) incrementCount(item Item, key int) {
-	list := cache.counter[item.Count]
-	list.Remove(cache.items[key])
+func (cache *LFUCache) increaseCount(item Item, key int) {
+	li := cache.counter[item.Count]
+	li.Remove(cache.items[key])
+	newCount := item.Count + 1
 
-	...
+	newCountList, exists := cache.counter[newCount]
+	if exists {
+		newCountList.PushBack(item)
+		return
+	}
+
+	newLi := list.New()
+	newLi.PushBack(item)
+	cache.counter[newCount] = newLi
 }
 
-
-func (this *LFUCache) Put(key int, value int)  {
+func (this *LFUCache) Put(key int, value int) {
 
 }
 
 func main() {
 
 }
-
