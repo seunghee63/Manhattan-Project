8a24123f246772555850c05fa44342f78f7152e8
AngJoong <conquerorj@naver.com>
2024-01-23 00:57:03 +0900

    Create SeatReservationManager.java

---
+++ week13/sj/SeatReservationManager.java
@@ -0,0 +1,57 @@
+class SeatManager {
+    private Heap availableSeats;
+    private Set<Integer> reservedSeats;
+
+    public SeatManager(int n) {
+        this.reservedSeats = new HashSet<>(n);
+        this.availableSeats = new Heap(n);
+        IntStream.range(1, n + 1)
+            .forEach(seatNumber -> availableSeats.push(seatNumber));
+    }
+
+    public int reserve() {
+        int targetSeat = this.availableSeats.pop();
+        this.reservedSeats.add(targetSeat);
+        return targetSeat;
+    }
+
+    public void unreserve(int seatNumber) {
+        this.reservedSeats.remove(seatNumber);
+        this.availableSeats.push(seatNumber);
+    }
+
+    private static class Heap {
+        private List<Integer> elements;
+
+        public Heap(int capacity) {
+            this.elements = new ArrayList<>(capacity);
+        }
+
+        public boolean isEmpty() {
+            return this.elements.isEmpty();
+        }
+
+        public void push(int value) {
+            this.elements.add(value);
+            swap();
+        }
+
+        public int pop() {
+            Integer element = this.elements.remove(0);
+
+            swap();
+
+            return element;
+        }
+
+        private void swap() {
+            for (int i = this.elements.size() - 1; i > 0; i--) {
+                if (elements.get(i/2) > this.elements.get(i)) {
+                    int temp = this.elements.get(i);
+                    this.elements.set(i , this.elements.get(i/2));
+                    this.elements.set(i/2, temp);
+                }
+            }
+        }
+    }
+}
