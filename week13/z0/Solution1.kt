fafc6f94aa9cf67f005f490dc9b59384d415f09f
z0 <ji9310@naver.com>
2024-01-23 20:52:09 +0900

    Create Solution1.kt

---
+++ week13/z0/Solution1.kt
@@ -0,0 +1,16 @@
+class Solution {
+    // 143ms, 37.84MB
+    fun lastStoneWeight(stones: IntArray): Int {
+        val queue = PriorityQueue<Int>(Collections.reverseOrder())
+        queue.addAll(stones.asList())
+
+        while(queue.size > 1) {
+            val stone1 = queue.poll()
+            val stone2 = queue.poll()
+            val result = stone1 - stone2
+            if(result > 0) queue.add(result)
+        }
+        return queue.poll()
+    }
+    
+}
