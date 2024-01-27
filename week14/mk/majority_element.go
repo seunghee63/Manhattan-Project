cb45b9fa18d31b3b74a9975408b35725760b56b0
10000-ki <10000ki6472@gmail.com>
2024-01-27 22:25:47 +0900

    resolve majority_element.go

---
+++ week14/mk/majority_element.go
@@ -0,0 +1,16 @@
+// 15ms, 6.26mb
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println(majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
+}
+
+func majorityElement(nums []int) int {
+	sort.Ints(nums)
+	return nums[len(nums)/2+len(nums)%2-1]
+}
