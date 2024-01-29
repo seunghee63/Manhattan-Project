a09eae455dc2336e5a023b62cbcd36b3757da73d
pizzazaza <riding1516@gmail.com>
2024-01-30 00:23:15 +0900

    Create InsertiionSortList.java

---
+++ week14/sejun/InsertiionSortList.java
@@ -0,0 +1,55 @@
+// 277ms, 49.68mb
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        int size = size(head);
+        ListNode cur = head;
+        for (int i = 0; i < size; i++) {
+            cur = injectionSort(cur);
+        }
+
+        return cur;
+    }
+
+    public ListNode injectionSort(ListNode node) {
+        if (node.next == null) {
+            return node;
+        }
+
+        ListNode next = injectionSort(node.next);
+        node.next = next;
+        if (node.val > next.val) {
+            swap(node, next);
+            return next;
+        }
+
+        return node;
+    }
+
+    public void swap(ListNode node, ListNode next) {
+        ListNode temp = next.next;
+        next.next = node;
+        node.next = temp;
+    }
+
+    public int size(ListNode head) {
+        int nodeSize = 1;
+        ListNode cur = head;
+        while(cur.next != null) {
+            nodeSize++;
+            cur = cur.next;
+        }
+
+        return nodeSize;
+    }
+}
