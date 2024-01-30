d0eafa2b618fb4f9e9677c28880249cb63190d05
Jung SeokJoon <eshc.joon@gmail.com>
2024-01-30 13:44:33 +0900

    Update InsertionSortList.kt

--- week14/seokjoon/InsertionSortList.kt
+++ week14/seokjoon/InsertionSortList.kt
@@ -5,7 +5,7 @@ class Solution {
         val newHeadNode = ListNode(-5001)
         while(curNode != null){
             var curNewNode = newHeadNode
-            while(curNewNode.next != null && curNewNode.next!!.`val` < curNode!!.`val` ){  // curNewNode의 `val`이 삽입할 Node의 `val`보다 크거나 null일 때까지 curNewNode 위치를 옮겨준다.
+            while(curNewNode.next != null && curNewNode.next!!.`val` < curNode!!.`val` ){  // curNewNode의 `val`이 삽입할 Node의 `val`보다 크거나 null일 때까지 curNewNode가 가리키는 node를 바꿔준다.
                 curNewNode = curNewNode.next
             }
             if(curNewNode.next == null){ //curNewNode.next가 null이면 next에 새로운 Node 추가
