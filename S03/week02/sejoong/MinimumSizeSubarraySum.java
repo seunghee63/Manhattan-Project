b13b9f9ab22d1fa5f4ba1dd2da3e40c38bd2f105
AngJoong <conquerorj@naver.com>
2024-04-08 18:32:09 +0900

    Update MinimumSizeSubarraySum.java

--- S03/week02/sejoong/MinimumSizeSubarraySum.java
+++ S03/week02/sejoong/MinimumSizeSubarraySum.java
@@ -1,21 +1,24 @@
 class Solution {
+    // Time Limit Exceeded T,.T
     public int minSubArrayLen(int target, int[] nums) {
-        List<Integer> sortedNums = Arrays.stream(nums)
-            .boxed()
-            .sorted(Comparator.reverseOrder())
-            .toList();
-
-        int sum = 0;
-        int subArrayLen = 0;
-        while (target > sum && subArrayLen < sortedNums.size()) {
-            sum += sortedNums.get(subArrayLen);
-            subArrayLen++;
+		int minSubArrayLen = 0;
+        // 순차 순회
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            // target보다 보다 크고 i를 subArray의 첫번째 요소로하는 subArray찾기
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                //subArray의 합이 target 보다 크면 length를 구해서 지금까지 구한 minSubArrayLen보다 작으면 length를 minSubArrayLen로 변경
+                if (sum >= target) {
+                    int length = j - i + 1;
+                    if (minSubArrayLen == 0 || minSubArrayLen > length) {
+                        minSubArrayLen = length;
+                    }
+                    break;
+                }
+            }
         }
-        
-        if (target > sum) {
-            return 0;
-        }
 
-        return subArrayLen;
+        return minSubArrayLen;
     }
 }
