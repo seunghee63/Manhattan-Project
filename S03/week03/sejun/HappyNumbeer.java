907f2417b23262a6938c5353c8c0acf60c1dc7a7
pizzazaza <riding1516@gmail.com>
2024-04-23 21:00:03 +0900

    Create HappyNumbeer.java

---
+++ S03/week03/sejun/HappyNumbeer.java
@@ -0,0 +1,38 @@
+// 1ms Beats 64.32% of users with Java
+
+class Solution {
+		Set<Integer> happyNumberSet = new HashSet<>();
+
+		public boolean isHappy(int n) {
+			int num = n;
+			boolean happy = false;
+
+
+			while (!happy) {
+				int div = 10;
+				int result = square(num % div);
+				while (num / div != 0) {
+					num = num / div;
+					int remainder = num % div;
+					result = result + square(remainder);
+				}
+
+				if (happyNumberSet.contains(result)) {
+					return false;
+				}
+				happyNumberSet.add(result);
+
+				num = result;
+
+				happy = num == 1;
+			}
+
+			return true;
+
+		}
+
+		public int square(int num) {
+			return num * num;
+		}
+
+	}
