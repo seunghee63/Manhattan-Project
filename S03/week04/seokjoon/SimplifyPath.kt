7e26af50f6c0f74b33598ace5feeb8a36b097da4
Jung SeokJoon <eshc.joon@gmail.com>
2024-04-30 08:53:56 +0900

    Create SimplifyPath.kt

---
+++ S03/week04/seokjoon/SimplifyPath.kt
@@ -0,0 +1,20 @@
+//209ms, 37.60mb
+class Solution {
+    fun simplifyPath(path: String): String {
+        val pathItemList = path.split("/")
+        val stack = Stack<String>()
+
+        for(pathItem in pathItemList){
+            if(pathItem == ".."){
+                if(stack.isNotEmpty()){
+                    stack.pop()
+                }
+            }else if(pathItem == "." || pathItem.isEmpty()) continue
+            else {
+                stack.push(pathItem)   
+            }
+        }
+        
+        return "/${stack.filter { it.isNotEmpty() }.joinToString("/")}"
+    }
+}
