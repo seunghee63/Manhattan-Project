a8dd8c104663579d065e81f31d5a3b98f8b2d55a
AngJoong <conquerorj@naver.com>
2024-05-05 16:34:39 +0900

    Update ClimbingStairs.kt

--- S03/week05/sejoong/ClimbingStairs.kt
+++ S03/week05/sejoong/ClimbingStairs.kt
@@ -1,12 +1,15 @@
+// Runtime 3991ms Beats 5.11% of users with Kotlin
+// Memory 32.98MB Beats 65.93% of users with Kotlin
 class Solution {
     fun climbStairs(n: Int): Int {
+        if (n <=3) return n
+
         return climb(n, 2) + climb(n, 1)
     }
 
     private fun climb(stairCount: Int, stepCount: Int) : Int {
         val remainedStairCount = stairCount - stepCount
-        if (remainedStairCount < 0) return 0
-        if (remainedStairCount == 0 || remainedStairCount == 1) return 1
+        if (remainedStairCount <= 3) return remainedStairCount
 
         return climb(remainedStairCount, 2) + climb(remainedStairCount, 1)
     }
